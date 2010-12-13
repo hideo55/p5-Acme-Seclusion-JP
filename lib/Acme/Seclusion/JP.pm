ef5fbf052c0ea823c651ec0d779d2f4b1c69f98b
hide_o_55 <hide.o.j55@gmail.com>
2010-12-13 22:00:09 +0900

    @INC のフック方式を変更

--- lib/Acme/Seclusion/JP.pm
+++ lib/Acme/Seclusion/JP.pm
@@ -4,6 +4,8 @@ use warnings;
 use Acme::CPANAuthors;
 use LWP::UserAgent;
 use Module::CoreList;
+use CPAN::DistnameInfo;
+use Devel::GlobalDestruction ();
 
 #preload
 use HTTP::Request;
@@ -20,16 +22,19 @@ our $VERSION = '0.01';
 
 my $COMPLETE = 0;
 my $UA;
+my %DIST_CACHE;
+
+my ( @ids_jp, @ids_cn, @ids_kr, @ids_nl, @ids_pt, @ids_all );
 
 BEGIN {
-	my @ids_jp = Acme::CPANAuthors->new('Japanese')->id;
-	my @ids_cn = Acme::CPANAuthors->new('Chinese')->id;
-	my @ids_kr = Acme::CPANAuthors->new('Korean')->id;
-	my @ids_nl = Acme::CPANAuthors->new('Dutch')->id;
-	my @ids_pt = Acme::CPANAuthors->new('Portuguese')->id;
-	
-	my @ids = (@ids_jp,@ids_cn, @ids_kr, @ids_nl, @ids_pt);
-	
+	@ids_jp = Acme::CPANAuthors->new('Japanese')->id;
+	@ids_cn = Acme::CPANAuthors->new('Chinese')->id;
+	@ids_kr = Acme::CPANAuthors->new('Korean')->id;
+	@ids_nl = Acme::CPANAuthors->new('Dutch')->id;
+	@ids_pt = Acme::CPANAuthors->new('Portuguese')->id;
+
+	@ids_all = ( @ids_jp, @ids_cn, @ids_kr, @ids_nl, @ids_pt );
+
 	$UA = LWP::UserAgent->new(
 		parse_head => 0,
 		env_proxy  => 1,
@@ -37,42 +42,57 @@ BEGIN {
 		timeout    => 30,
 	);
 
-	unshift @INC, sub {
-		my $mod = $_[1];
-		$mod =~ s!/!::!g;
-		$mod =~ s/\.pm$//;
-		unless ($Module::CoreList::version{$]}{$mod}) {
-			my $id = get_author_id($mod);
-			my $ids = $COMPLETE ? \@ids_jp : \@ids;
-			unless ( defined($id) && grep { /^$id$/ } @$ids ) {
-				print STDERR qq{As for the $mod module, the import is prohibited by seclusion policy\n};
-				CORE::exit(1);
-			}
+	unshift @INC, ( bless {}, __PACKAGE__ );
+}
+
+sub Acme::Seclusion::JP::INC {
+	my ( $code, $mod_name, $mod_path ) = @_;
+	$mod_name =~ s!/!::!g;
+	$mod_name =~ s/\.pm$//;
+	unless ( $Module::CoreList::version{$]}{$mod_name} ) {
+		my $dist = get_dist($mod_name);
+		return if $DIST_CACHE{$dist};
+		my $id = get_author_id($dist);
+		my $ids = $COMPLETE ? \@ids_jp : \@ids_all;
+		unless ( defined($id) && grep { /^$id$/ } @$ids ) {
+			print STDERR
+qq{As for the $mod_name module, the import is prohibited by seclusion policy!\n};
+			CORE::exit(1);
 		}
-	};
+		else {
+			$DIST_CACHE{$dist} = 1;
+		}
+	}
+}
+
+sub DESTROY {
+	return if Devel::GlobalDestruction::in_global_destruction();
+	print STDERR qq{Smuggling is prohibited!\n};
+	exit(1);
 }
 
 sub import {
 	my $class = shift;
-	if( @_ == 1 && $_[0] eq '-complete'){
+	if ( @_ == 1 && $_[0] eq '-complete' ) {
 		$COMPLETE = 1;
 	}
 }
 
-sub get {
+sub get_dist {
 	my $mod = shift;
 	my $uri = "http://search.cpan.org/perldoc?$mod";
 	my $res = $UA->request( HTTP::Request->new( GET => $uri ) );
 	return unless $res->is_success;
-	return $res->decoded_content;
+	my $html = $res->decoded_content;
+	my ($dist) = $html =~
+	  m!<a href="/CPAN/authors/id/(.*?\.(?:tar\.gz|tgz|tar\.bz2|zip))">!;
+	return $dist;
 }
 
 sub get_author_id {
-	my $mod  = shift;
-	my $html = get($mod);
-	return unless $html;
-	$html =~ m!<a href="/CPAN/authors/id/(.*?\.(?:tar\.gz|tgz|tar\.bz2|zip))">!;
-	return $1;
+	my $dist = shift;
+	my $d    = CPAN::DistnameInfo->new($dist);
+	return $d->cpanid;
 }
 
 1;
